924ba8185ee3fac7ae4547c7c30a3621c88d99ae
fernando <fernandocorinaldesi@hotmail.com>
2020-04-16 03:02:45 -0300

    Primer commit

---
+++ MultiplicacionMatricesSecuencial/MultiplicacionMatricesSecuencial/Matriz.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicacionMatricesSecuencial
+{
+    class Matriz
+    {
+        private int filas;
+        private int columnas;
+        private int[,] elementos;
+        Random aleatorio = new Random();
+        public Matriz(int filas, int columnas)
+        {
+            this.filas = filas;
+            this.columnas = columnas;
+            elementos = new int[this.filas, this.columnas];
+        }
+
+        public void Mostrar()
+        {
+            for (int f = 0; f < filas; f++)
+            {
+                for (int c = 0; c < columnas; c++)
+                {
+                    Console.Write(elementos[f, c] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public void Cargar()
+        {
+            for (int f = 0; f < filas; f++)
+            {
+                for (int c = 0; c < columnas; c++)
+                {
+
+                    elementos[f, c] = aleatorio.Next(0, 9);
+                }
+            }
+        }
+
+        private bool EsMultiplicablePor(Matriz otra)
+        {
+            return (this.columnas == otra.filas);
+        }
+
+        public Matriz Multiplicar(Matriz otra)
+        {
+            if (EsMultiplicablePor(otra))
+            {
+                return this.MultiplicarPor(otra);
+            }
+            else
+            {
+                throw new Exception("No se pudo realizar la multiplicacion: A.columnas != B.filas");
+            }
+        }
+
+        private Matriz MultiplicarPor(Matriz otra)
+        {
+            Matriz resultado = new Matriz(this.filas, otra.columnas);
+            int respuesta = 0;
+
+            Parallel.For(0, this.filas, i => {
+                for (int j = 0; j < otra.columnas; j++)
+                {
+                    for (int k = 0; k < this.columnas; k++)
+                    {
+                        respuesta += this.elementos[i, k] * otra.elementos[k, j];
+                    }
+                    resultado.elementos[i, j] = respuesta;
+                    respuesta = 0;
+                }
+            });
+            return resultado;
+        }
+    }
+}
--- MultiplicacionMatricesSecuencial/MultiplicacionMatricesSecuencial/Program.cs
+++ MultiplicacionMatricesSecuencial/MultiplicacionMatricesSecuencial/Program.cs
@@ -9,76 +9,63 @@ namespace MultiplicacionMatricesSecuencial
 {
     class Program
     {
-        static int[,] generateMatrix(int rows, int columns)
-        {
-            Random random = new Random();
-            int[,] matrix = new int[rows, columns]; //Matriz de 3 filas x 2 columnas
-            for (int r = 0; r < rows; r++)
-            {
-                for (int c = 0; c < columns; c++)
-                {
-                    matrix[r, c] = random.Next(1, 100); ;
-                }
-            }
-
-            return matrix;
-        }
+    
 
         static void Main(string[] args)
         {
-            int respuesta = 0;
-
-            int r1 = 900;
-            int c1 = 400;
-            int[,] matrix1 = generateMatrix(r1, c1); //Matriz de 3 filas x 2 columnas
-
-            int r2 = 400;
-            int c2 = 900;
-            int[,] matrix2 = generateMatrix(r2, c2); //Matriz de 2 filas x 3 columnas
-
-            if (c1 == r2) //Si el numero de columnas de la 1 es igual al numero de filas de la 2
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            try
             {
+                Matriz m1 = new Matriz(2, 3);
+                Matriz m2 = new Matriz(3, 2);
 
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-
-                int[,] matrixResponse = new int[r1, c2];
-
-                for (int i = 0; i < r1; i++)
+                Task t1 = new Task(() =>
                 {
-                    for (int j = 0; j < c2; j++)
-                    {
-                        for (int k = 0; k < c1; k++)
-                        {
-                            respuesta += matrix1[i, k] * matrix2[k, j];
-                        }
-                        matrixResponse[i, j] = respuesta;
-                        respuesta = 0;
-                    }
-                }
+                    m1.Cargar();
 
-                sw.Stop();
-
-                Console.WriteLine("LA RESPUESTA ES: ");
-                string igual = "";
-                for (int i = 0; i < r1; i++)
+                });
+                Task t2 = new Task(() =>
                 {
-                    for (int j = 0; j < c2; j++)
-                    {
-                        igual = igual + (matrixResponse[i, j].ToString()) + "  ";
-                    }
-                    Console.WriteLine("[  " + igual + "]");
-                    igual = "";
-                }
+                    m2.Cargar();
 
-                Console.WriteLine("El programa demoró " + (sw.ElapsedMilliseconds / 1000) + "seg.");
+                });
+                Task t3 = new Task(() =>
+                {
+                    Console.WriteLine("Matriz A");
+                    m1.Mostrar();
+                    Console.WriteLine("\nMatriz B");
+                    m2.Mostrar();
+
+                });
+
+                Task t4 = new Task(() =>
+                {
+                    Console.WriteLine("\nResultado");
+                    m1.Multiplicar(m2).Mostrar();
+                });
+
+
+                t1.Start();
+                t2.Start();
+                Task.WaitAll(t1, t2);
+
+                t3.Start();
+                t3.Wait();
+
+                t4.Start();
+                t4.Wait();
 
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("No se puede multiplicar estas matrices");
-                Console.Read();
+                Console.WriteLine(e.Message);
             }
+
+            sw.Stop();
+            Console.WriteLine("El programa demoró " + (sw.ElapsedMilliseconds / 1000) + " seg.");
+            Console.WriteLine("Presione enter para finalizar");
+            Console.ReadLine();
 
         }
     }
